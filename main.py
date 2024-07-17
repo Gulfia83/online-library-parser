32d4bb86f7c4729de32d5f58baa48740589ac2ee
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-07-17 17:33:26 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import os
 from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
 
 
 def check_for_redirect(response):
@@ -8,28 +11,44 @@ def check_for_redirect(response):
         raise HTTPError('Redirection occurred')
     
 
-def download_book(response, path):
-    with open(path, 'wb') as file:
+def get_book_title(book_url):
+    response = requests.get(book_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1')
+    if title_tag:
+        title_text = title_tag.text.split('::')
+        title = title_text[0].strip()
+    else:
+        print("Не удалось найти заголовок.")
+
+    return title
+
+
+def download_txt(response, title,book_id, folder='books/'):
+    sanitized_title = sanitize_filename(title)
+    filename = f'{book_id}.{sanitized_title}.txt'
+    with open(os.path.join(folder, filename), 'wb') as file:
         file.write(response.content)
 
 
 os.makedirs('books', exist_ok=True)
-url = f'https://tululu.org/txt.php'
+download_url = f'https://tululu.org/txt.php'
 
 for book_id in range(1, 11):
+    book_url = f'https://tululu.org/b{book_id}/'
     params = {
     'id': book_id
     }
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(download_url, params=params)
         response.raise_for_status()
         check_for_redirect(response)
     except HTTPError:
         print('Error occured')
         continue
 
-    download_book(response=response,
-                  path=f'books/id {book_id}.txt')
-
-    
-   
+    title = get_book_title(book_url)
+    download_txt(response,
+                  title,
+                  book_id)
