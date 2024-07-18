91395e2bd80bae6b15fd3691473893ea22a22502
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-07-18 11:54:36 +0300

    download comments.

--- main.py
+++ main.py
@@ -17,11 +17,8 @@ def get_book_title(book_url):
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find('h1')
-    if title_tag:
-        title_text = title_tag.text.split('::')
-        title = title_text[0].strip()
-    else:
-        print("Не удалось найти заголовок.")
+    title_text = title_tag.text.split('::')
+    title = title_text[0].strip() if title_tag else None
 
     return title
 
@@ -31,13 +28,20 @@ def get_image_url(book_url):
     response = requests.get(book_url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    book_image_src = soup.find(class_='bookimage').find('img')['src']
-    if book_image_src:
-        image_url = urljoin(base_url, book_image_src)
-    else:
-        print("Не удалось найти ссылку на картинку")
+    book_img_src = soup.find(class_='bookimage').find('img')['src']
+    img_url = urljoin(base_url, book_img_src) if book_img_src else None
 
-    return image_url
+    return img_url
+
+
+def get_comments(book_url):
+    response = requests.get(book_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    comments_elements = soup.find_all('div', class_='texts')
+    comments = [comment.find('span', class_='black').text for comment in comments_elements]
+
+    return comments
 
 
 def download_txt(response, title,book_id, folder='books/'):
@@ -47,16 +51,17 @@ def download_txt(response, title,book_id, folder='books/'):
         file.write(response.content)
 
 
-def download_image(image_url, book_id, folder='images/'):
+def download_image(img_url, book_id, folder='images/'):
     response = requests.get(image_url)
     response.raise_for_status()
 
-    split_url = urlsplit(image_url)
+    split_url = urlsplit(img_url)
     path = unquote(split_url.path)
     extension = path.split('.')[-1] if '.' in path else None
-    image_name = f'{book_id}.{extension}' if 'nopic.gif' not in path else 'nopic.gif'
-    with open(os.path.join(folder, image_name), 'wb') as file:
+    img_name = f'{book_id}.{extension}' if 'nopic.gif' not in path else 'nopic.gif'
+    with open(os.path.join(folder, img_name), 'wb') as file:
         file.write(response.content)
+
 
 os.makedirs('books', exist_ok=True)
 os.makedirs('images', exist_ok=True)
@@ -73,13 +78,14 @@ for book_id in range(1, 11):
         response.raise_for_status()
         check_for_redirect(response)
     except HTTPError:
-        print('Error occured')
         continue
 
     title = get_book_title(book_url)
     download_txt(response,
                   title,
                   book_id)
-    image_url = get_image_url(book_url)
-    download_image(image_url,
+    image_url = get_img_url(book_url)
+    download_image(img_url,
                    book_id)
+    comments = get_comments(book_url)
+
